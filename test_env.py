8494b6c32869c7d890504a7d614fbafa3b0f1eca
AleexSilva <silva.alexis94@gmail.com>
2025-02-24 22:34:53 -0300

    Check if the API is OK

--- test_env.py
+++ test_env.py
@@ -11,3 +11,14 @@ print(dotenv_values())  # Should display variables from .env
 # Try accessing a specific variable
 api_key = os.getenv("OPENAI_API_KEY")
 print(f"API Key: {api_key}")
+
+# Check the key
+
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
